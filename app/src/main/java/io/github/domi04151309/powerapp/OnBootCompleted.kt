ae4e90f580757fab0ea4cd9484bc0116c36faa29
Domi <domi04151309business@gmail.com>
2019-07-01 18:45:28 +0200

    Activate qs tiles on boot

---
+++ app/src/main/java/io/github/domi04151309/powerapp/OnBootCompleted.kt
@@ -0,0 +1,22 @@
+package io.github.domi04151309.powerapp
+
+import android.content.BroadcastReceiver
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.TileService
+import io.github.domi04151309.powerapp.qs.Reboot
+import io.github.domi04151309.powerapp.qs.RestartSystemUI
+import io.github.domi04151309.powerapp.qs.Shutdown
+
+class OnBootCompleted : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if(intent.action == Intent.ACTION_BOOT_COMPLETED && Build.VERSION.SDK_INT >= 24) {
+            TileService.requestListeningState(context, ComponentName(context , Shutdown::class.java))
+            TileService.requestListeningState(context, ComponentName(context , Reboot::class.java))
+            TileService.requestListeningState(context, ComponentName(context , RestartSystemUI::class.java))
+        }
+    }
+}
