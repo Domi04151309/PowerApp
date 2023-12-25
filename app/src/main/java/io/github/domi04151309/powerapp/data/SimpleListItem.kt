d4a1358b571643918a2cb5a0d133e218fbb061e3
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2023-12-25 21:23:53 +0100

    Use recycler view

---
+++ app/src/main/java/io/github/domi04151309/powerapp/data/SimpleListItem.kt
@@ -0,0 +1,7 @@
+package io.github.domi04151309.powerapp.data
+
+data class SimpleListItem(
+    val title: String = "",
+    val summary: String = "",
+    val icon: Int = android.R.color.transparent,
+)
